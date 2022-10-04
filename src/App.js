b5fab5dc64dd3661a691f57cd6ba8a7de2e3b8fb
David-Erick <108961323+David-Erick@users.noreply.github.com>
2022-10-04 15:50:17 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//components
+import FirstComponent from './components/FirstComponent';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>David Erick</h1>
+    <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
